ecf0abb8fce952269c717524045efd32f42cbbea
gonzalovazquez <gonzalo.segura1@gmail.com>
2016-04-01 15:41:34 -0400

    feature: Ability to fetch all questions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
+const Boom = require('boom');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -8,6 +10,47 @@ server.connection({
     host: 'localhost',
     port: 8000
 });
+
+// Database variables
+const username = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
+
+console.log(process.env.USER, process.env.PASSWORD, process.env.DATABASE);
+
+// MongoDB connection
+const dbOpts = {
+  "url" : "mongodb://" + username + ":" + password + "@ds011810.mlab.com:11810/" + database,
+  "settings": {
+    "db": {
+      "native_parser": false
+    }
+  }
+};
+
+server.register({
+  register: require('hapi-mongodb'),
+  options: dbOpts
+}, function (err) {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+});
+
+// Fetch a questions
+server.route( {
+  "method": "GET",
+  "path": "/allquestions",
+  config: {
+			handler: (request, reply) => {
+				var db = request.server.plugins['hapi-mongodb'].db;
+				reply(db.collection('questions').find({}).toArray());	//filter only with ID + TITLE
+			},
+			cors: true
+		}
+});
+
 
 // Default route
 server.route({
@@ -18,32 +61,47 @@ server.route({
   }
 });
 
-// Name
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function(request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
 
 
-
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, reply) {
-
-        return reply('hello world');
-    }
-});
-
-// Start the server
-server.start((err) => {
+// Static pages and content
+server.register(require('inert'), (err) => {
 
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info);
+
+    server.route({
+        method: 'GET',
+        path: '/say',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+// Logging
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+           throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
